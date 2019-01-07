ebd9a563bb9682c45ab3ebe751a6cad5bedd39d2
Izabela Kuzniar <iza8848@gmail.com>
2019-01-07 12:45:35 +0100

    Add project files.

---
+++ TCPwithSSLExamDiceClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPwithSSLExamDiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
